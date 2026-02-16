608b57852992de4019dc71e668e0af30411d4e61
Rai <raidasilva1992@gmail.com>
2026-02-16 10:48:42 -0300

    finalizado ex03

---
+++ Shell01/ex03/count_files.sh
@@ -0,0 +1,15 @@
+find . | wc -l
+
+: '
+comando: find .
+→ lista tudo dentro da pasta atual
+→ inclui:
+	.
+	subpastas
+	arquivos
+
+wc -l
+→ conta quantas linhas foram listadas
+→ cada linha = 1 arquivo ou pasta
+
+Como o find . já inclui o . automaticamente, ele já conta a pasta inicial também'
